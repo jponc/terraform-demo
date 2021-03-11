a4c97ba37273765abbe48fa57aef22c083fc7818
Julian Alfonso Ponce <ponce.julianalfonso@gmail.com>
2021-03-11 15:44:00 +1100

    Added aws ec2 instance via tf

---
+++ learn-terraform-aws-instance/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "uptactics"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
